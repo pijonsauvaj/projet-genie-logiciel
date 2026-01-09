c3d98b5bc45576d4bde4fe074cf2187410e8d414
Thierry <thierry2003email.fr@gmail.com>
2026-01-09 17:31:25 +0100

    Initial commit

---
+++ src/main/java/fr/uparis/projet_genie_log/App.java
@@ -0,0 +1,13 @@
+package fr.uparis.projet_genie_log;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
