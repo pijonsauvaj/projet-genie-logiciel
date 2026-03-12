734a79cbe87acc1c62aea9db2be2d4ebc2645a08
Thierry <thierry2003email.fr@gmail.com>
2026-03-12 22:59:44 +0100

    CLI continue

--- src/main/java/fr/uparis/projet_genie_logiciel/presentation/CLI.java
+++ src/main/java/fr/uparis/projet_genie_logiciel/presentation/CLI.java
@@ -3,15 +3,18 @@ package fr.uparis.projet_genie_logiciel.presentation;
 
 import java.util.Scanner;
 
+import fr.uparis.projet_genie_logiciel.domain.model.Category;
+import fr.uparis.projet_genie_logiciel.domain.service.CategoryService;
 import fr.uparis.projet_genie_logiciel.domain.service.ProductService;
 
 public class CLI {
-
+	private CategoryService categoryService;
     private ProductService service;
     private Scanner scanner;
 
-    public CLI(ProductService service) {
-        this.service = service;
+    public CLI(ProductService service, CategoryService categoryService) {
+    	this.categoryService = categoryService;
+    	this.service = service;
         this.scanner = new Scanner(System.in);
     }
 
@@ -50,6 +53,22 @@ public class CLI {
         System.out.println("3. Sortie de stock");
         System.out.println("0. Quitter");
         System.out.print("Votre choix : ");
+    }
+    
+    private void productRegister() { //Y'a un truc qui va pas au niveau des categories
+        System.out.print("Nom du produit: \n");
+        String name = scanner.nextLine();
+
+        System.out.print("\n\nQuantité du produit: \n");
+        int quantity = Integer.parseInt(scanner.nextLine());
+        
+        System.out.print("Dans quel catégorie voulez vous ajouter le produit? \n");
+        String nameCategory = scanner.nextLine();
+        Category category = new Category(nameCategory);
+        categoryService.addCategory(nameCategory);
+
+        service.addProduct(name, quantity, category); //je dois ajouter une categorie mais je dois d'abord demander le nom puis...
+        System.out.println("Entrée enregistrée.");
     }
 	
 }
