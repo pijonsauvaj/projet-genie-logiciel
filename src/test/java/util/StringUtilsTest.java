be1c8b630c8ed50fdc8d88c233fcf9d2d9566304
pijonsauvaj <thierry2003email.fr@gmail.com>
2026-04-09 20:32:46 +0200

    add another test for util

--- src/test/java/util/StringUtilsTest.java
+++ src/test/java/util/StringUtilsTest.java
@@ -1,6 +1,7 @@
 package util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,8 @@ public class StringUtilsTest {
 	void lowerCaseShouldNormalizeString() {
 	    assertEquals("tomate", StringUtils.lowerCase("  ToMaTe  "));
 	}
-
+    @Test
+    void lowerCaseShouldReturnNullWhenInputIsNull() {
+        assertNull(StringUtils.lowerCase(null));
+    }
 }
