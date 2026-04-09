ede2bac493c77b80c9655745014c2792a3db8a44
dioni985 <dioni985123@gmail.com>
2026-04-09 11:56:24 +0200

    one less checkstyle error

--- src/main/java/fr/uparis/projet_genie_logiciel/presentation/command/AddProductCommand.java
+++ src/main/java/fr/uparis/projet_genie_logiciel/presentation/command/AddProductCommand.java
@@ -18,27 +18,25 @@ public class AddProductCommand implements Command {
 
 	@Override
 	public void execute() {
-	    System.out.print("Name: ");
-	    String name = scanner.nextLine();
-	    System.out.print("Quantity: ");
-	    int quantity = Integer.parseInt(scanner.nextLine());
-	    System.out.print("Category: ");
-	    String nameCategory = scanner.nextLine();
-	    boolean newCategory = categoryService.addCategory(nameCategory);
-	    boolean newProduct = service.addProduct(name, quantity, nameCategory);
-	    if (newCategory) {
-	        System.out.println("Category created.");
-	    } else {
-	        System.out.println("Existing category.");
-	    }
-	    if (newProduct) {
-	        System.out.println("Product added.");
-	    } else {
-	        System.out.println("Existing product: quantity increased.");
-	    }
+		System.out.print("Name: ");
+		String name = scanner.nextLine();
+		System.out.print("Quantity: ");
+		int quantity = Integer.parseInt(scanner.nextLine());
+		System.out.print("Category: ");
+		String nameCategory = scanner.nextLine();
+		boolean newCategory = categoryService.addCategory(nameCategory);
+		boolean newProduct = service.addProduct(name, quantity, nameCategory);
+		if (newCategory) {
+			System.out.println("Category created.");
+		} else {
+			System.out.println("Existing category.");
+		}
+		if (newProduct) {
+			System.out.println("Product added.");
+		} else {
+			System.out.println("Existing product: quantity increased.");
+		}
 	}
-
-
 
 	@Override
 	public String getName() {
